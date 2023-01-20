cb17b7b6c1e201f1b59e44ed577b32453a239ac2
KyungminMoon <km990106@gmail.com>
2023-01-20 16:23:36 +0900

    0120

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,11 @@
 from django import forms 
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-class modelsForm(forms.ModelForm):
+class UserForm(UserCreationForm):
     title = forms.CharField(min_length=2, max_length=100)
-    
-
     class Meta:
         model = User
-        fields = ('ID_user', 'ID_user',)
+        fields = ('ID_user', 'PW_user')
